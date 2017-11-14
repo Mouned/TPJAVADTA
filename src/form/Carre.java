0634602b0b4c9b0d8f57e77cd730172f52e75d8b
mouned <moussa.nedjari92@gmail.com>
2017-11-14 17:27:31 +0100

    test

--- src/form/Carre.java
+++ src/form/Carre.java
@@ -4,7 +4,6 @@ import interfaceUtils.Surfacable;
 
 public class Carre extends Rectangle implements Surfacable{
 
-
 	public Carre(Point pointBasGauche, int cote) {
 		super(pointBasGauche,cote,cote);
 	}
